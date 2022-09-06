fd6a396863df38599d9d733228a7709d1f7dfe83
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2022-09-06 23:40:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*codingmaker_*/
+document.querySelector("#open-popup").addEventListener("click",function(){
+   document.querySelector(".popup").classList.add("active");
+ });
+
+ document.querySelector(".popup .close-btn").addEventListener("click",function(){
+   document.querySelector(".popup").classList.remove("active");
+ });
+
